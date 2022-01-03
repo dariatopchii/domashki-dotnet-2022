d4c744ec6939643a1003b6c922c343b478990465
dariatopchii <96009341+dariatopchii@users.noreply.github.com>
2022-01-03 11:04:56 +0200

    file manager

---
+++ dotnet 2022 study/FileManager.cs
@@ -0,0 +1,231 @@
+using System;
+using System.IO;
+using System.Linq;
+
+namespace dotnet_2022_study
+{
+    public static class FileManager
+    {
+        public static string Path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+        public static void CreateFile(string filename)
+        {
+            if (!File.Exists($@"{Path}\{filename}"))
+            {
+                using (File.Create($@"{Path}\{filename}"))
+                {
+                }
+                Console.WriteLine($"{filename} created in {Path}");
+            }
+            else
+            {
+                Console.WriteLine($"{filename} already exists in {Path}");
+            }
+        }
+
+        public static void DeleteFile(string filename)
+        {
+            var filepath = $@"{Path}\{filename}";
+            if (File.Exists(filepath))
+            {
+                File.Delete(filepath);
+                Console.WriteLine($"{filename} deleted in {Path}");
+            }
+            else
+            {
+                Console.WriteLine($"{filename} doesn`t exist in {Path}");
+            }
+        }
+
+        public static void RenameFile(string oldname, string newname )
+        {
+            if (File.Exists($@"{Path}\{oldname}"))
+            {
+                File.Move($@"{Path}\{oldname}", $@"{Path}\{newname}");
+                Console.WriteLine($"{oldname} changed to {newname}");
+            }
+            else
+            {
+                Console.WriteLine($"{oldname} file doesn`t exist in {Path}");
+            }
+        }
+        
+        
+        public static void CreateDirectory(string dirname)
+        {
+            var newDir = $@"{Path}\{dirname}";
+            if (!Directory.Exists(newDir) && !File.Exists(newDir))
+            {
+                Directory.CreateDirectory(newDir);
+                Console.WriteLine($" {newDir} created");
+            }
+            else
+            {
+                Console.WriteLine($"{newDir} already exists");
+            }
+           
+        }
+        
+        public static void DeleteDirectory(string dirname)
+        {
+            var dirPath = $@"{Path}\{dirname}";
+            if (Directory.Exists(dirPath))
+            {
+                Directory.Delete(dirPath);
+                Console.WriteLine($" {dirPath} deleted");
+            }
+            else
+            {
+                Console.WriteLine($"directory {dirPath} doesn`t exist");
+            }
+        }
+        
+        public static void RenameDirectory(string oldname, string newname)
+        {
+            var oldpath = $@"{Path}\{oldname}";
+            var newpath = $@"{Path}\{newname}";
+            if (Directory.Exists(oldpath))
+            {
+                Directory.Move(oldpath, newpath);
+                Console.WriteLine($"{oldname} changed to {newname}");
+            }
+            else
+            {
+                Console.WriteLine($" {oldname} directory doesn`t exist");
+            }
+
+        }
+
+        public static void ChangeDirectory(string[] commline)
+        {
+            if (commline.Length > 1)
+            {
+                if (commline[1] == ".." && commline.Length == 3 && Directory.Exists($@"{Path}\{commline[2]}"))
+                {
+                    Path = $@"{Path}\{commline[2]}";
+                }
+                else if (Directory.Exists(commline[1]))
+                {
+                    Path =  commline[1];
+                }
+                else if(commline[1] == ".." && commline.Length <3) 
+                {
+                    var cut = Path.LastIndexOf("\\");
+                    Path = Path.Substring(0, cut);
+                    Console.WriteLine(Path);
+                }
+                else
+                {
+                    Console.WriteLine("there is no directory with such path");
+                }
+            }
+           
+        }
+
+        public static void DirectoryInfo(string[] input)
+        {
+            if (input.Length > 1)
+            {
+                switch (input[1])
+                {
+                   case "-s" : Print(Directory.GetFiles(Path).OrderBy(f => new FileInfo(f).Length));
+                       break;
+                   case "-t" : Print(Directory.GetFiles(Path).OrderBy(f => new FileInfo(f).Extension));
+                       break;
+                   case "-n": Print(Directory.GetFiles(Path).OrderBy(f => new FileInfo(f).Name));
+                       break;
+                   case "-h":
+                       DirectoryInfo directory = new DirectoryInfo(Path);
+                       FileInfo[] file = directory.GetFiles();
+                       var filtered = file.Where(f => !f.Attributes.HasFlag(FileAttributes.Hidden));
+                       foreach (var f in filtered)
+                       {
+                          Console.WriteLine(f);
+                       }
+                       break;
+                   case "-a" :  var files = Directory.GetFiles(Path);
+                       Console.WriteLine(Path);
+                       foreach (var y in files)
+                       {
+                           var a = new FileInfo(y);
+                           Console.WriteLine(
+                               $"{y} \n -size: {a.Length} b \n -date created: {a.CreationTime} \n -last accessed: {a.LastAccessTime} \n");
+                       }
+                       break;
+                   default: Console.WriteLine("wrong input");
+                       break;
+                }
+            }
+            else
+            {
+                var files = Directory.GetFileSystemEntries(Path);
+                foreach (var y in files)
+                {
+                    Console.WriteLine(y.Substring(Path.Length));
+                }
+            }
+        }
+        
+        public static void Print(IOrderedEnumerable<string> sorted)
+        {
+            Console.WriteLine(Path);
+            foreach (var s in sorted) Console.WriteLine(s);
+        }
+
+        public static void FindSubstring(string path, string substring)
+        {
+            string filepath = $@"{Path}\{path}";
+            using (StreamReader sr = File.OpenText(filepath))
+            {
+                string lines = File.ReadAllText((filepath));
+                bool isMatch = false;
+                for (int x = 0; x < lines.Length - 1; x++)
+                {
+                    if (lines.Contains(substring))
+                    {
+                        sr.Close();
+                        Console.WriteLine("there is a match");
+                        isMatch = true;
+                        break;
+                    }
+                }
+                if (!isMatch)
+                {
+                    sr.Close();
+                    Console.WriteLine("there is no match");
+                }
+            }
+            
+        }
+
+        public static void PrintFile(string path)
+        {
+
+            using (StreamReader file = new StreamReader($@"{Path}\{path}"))
+            {
+                string ln;
+
+                while ((ln = file.ReadLine()) != null && ln.Length <= 200)
+                {
+                    Console.WriteLine(ln);
+                }
+
+                if (ln.Length < 200)
+                {
+                    Console.WriteLine("File has less than 200 symbols");
+                }
+
+                file.Close();
+            }
+        }
+
+
+
+        public static void Help()
+        {
+            Console.WriteLine("available commands: \n mkdir - creates directory \n rmdir - deletes directory \n rndir - renames directory \n cd - changes directory \n ls - show all files in directory " +
+                              "\n     -a  - shows all files and their properties \n     -s - sorts all files by size \n     -t - sorts all files by extention \n     -h - hides hidden files " +
+                              "\n newfl - creates a new file \n del - deletes file \n sub - searches for a substring \n print - prints first 200 symbols  \n quit - quits");
+        }
+    }
+}
---
+++ dotnet 2022 study/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace dotnet_2022_study
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Welcome to the file manager. Type 'help' if you want to see the list of commands");
+            var cmmd = Console.ReadLine();
+            while (cmmd != "quit"){
+                string[] commd = cmmd?.Replace(@"\s+", " ").Split(" ");
+                switch (commd?[0])
+                {
+                    case "mkdir":
+                        if (commd.Length > 1) FileManager.CreateDirectory(commd[1]);
+                        break;
+                    case "rmdir" :
+                        if (commd.Length > 1) FileManager.DeleteDirectory(commd[1]);
+                        break;
+                    case "rndir" :
+                        if (commd.Length > 2) FileManager.RenameDirectory(commd[1], commd[2]);
+                        break;
+                    case "cd": FileManager.ChangeDirectory(commd);
+                        break;
+                    case "newfl" :
+                        if (commd.Length > 1) FileManager.CreateFile(commd[1]);
+                        break;  
+                    case "rnfl" :
+                        if (commd.Length > 2) FileManager.RenameFile(commd[1], commd[2]);
+                        break;
+                    case "del"  :
+                        if (commd.Length > 1) FileManager.DeleteFile(commd[1]);
+                        break;
+                    case "ls" : FileManager.DirectoryInfo(commd);
+                        break;
+                    case "sub" :
+                        if (commd.Length > 2) FileManager.FindSubstring(commd[1], commd[2]);
+                        break;
+                    case "print" :
+                        if (commd.Length > 1) FileManager.PrintFile(commd[1]);
+                        break;
+                    case "help": FileManager.Help();
+                        break;
+                    default: Console.WriteLine("wrong input");
+                        break;
+                }
+                Console.Write(FileManager.Path + "> ");
+                cmmd = Console.ReadLine();
+            }
+        }
+
+    }
+}
